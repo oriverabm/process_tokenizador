425e8c0306a3a090a303f05eaa1d9da938ff9ab5
Esaud Rivera Lagos <Esaud17@users.noreply.github.com>
2023-02-22 17:19:24 -0600

    other retry

---
+++ src/contrato.no-found-ids.ts
@@ -0,0 +1,85 @@
+import { URL_BASE } from '../config/settings';
+import { instanceMongoSSH } from '../database/mongodb';
+import * as MySQLConnector from '../database/mysql';
+import fetch from 'node-fetch';
+
+(async () => {
+  const { db, client } = await instanceMongoSSH();
+
+  const sliceIntoChunks = (arr: any, chunkSize: any) => {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      const chunk = arr.slice(i, i + chunkSize);
+      res.push(chunk);
+    }
+    return res;
+  };
+
+  const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+  MySQLConnector.init();
+
+  try {
+    const collectionContracts = db.collection('contracts');
+
+    const query = `SELECT contrato_id,status 
+                   FROM vcm.card_migrations
+                   WHERE 1 = 1;`;
+
+    const contractsTokenizados: any[] = await MySQLConnector.execute(query, []);
+
+    let findquery = contractsTokenizados.map((x) => x.contrato_id);
+
+    const sendContracts = await collectionContracts
+      .find({ contratoId: { $nin: findquery } })
+      .toArray();
+
+    let noFoundNin = sendContracts.map((y) => y.contratoId);
+
+    console.log(noFoundNin);
+
+    //let payload = {
+    //  contracts_pool: sends,
+    //  tokenized_contracts: tokenizados,
+    //};
+
+    // const response = await fetch(`${URL_BASE}/api/get-contracts-not-tokenized`,
+    //   {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     method: 'POST',
+    //     body: JSON.stringify(payload),
+    //   }
+    // ).then((res) => {
+    //   let result;
+    //   if (res.ok) result = res.text();
+    //   else {
+    //     result = res.status;
+    //     console.log('respose no ok:', result);
+    //   }
+    //   return result;
+    // });
+
+    let contractsRetry = noFoundNin;
+
+    console.log('contractsRetry', contractsRetry.length);
+
+    console.log('############### START EXCUTE ##################\n');
+
+    for (let contract of contractsRetry) {
+      console.log(contract);
+      await collectionContracts.updateOne(
+        { contratoId: Number(contract) },
+        { $set: { status: 'active' } }
+      );
+    }
+
+    console.log('############### END EXCUTE ##################\n');
+  } catch (error) {
+    console.log('error', error);
+  } finally {
+    console.log('closing');
+    await client.close();
+  }
+})();
